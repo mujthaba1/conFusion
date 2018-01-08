43d79d11d2d6aee4d19faac48ef7fb0431153445
Mujthaba Khattal <mujthabakhattal@outlook.com>
2018-01-08 17:22:32 +0400

    NPM Script Part 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    //$('[data-toggle="tooltip"]').tooltip();
+    $().button('toggle');
+    //$('[data-toggle="buttons"]').button();
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) 
+          {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play'
+              );
+            $("#carousel-button").children("span").addClass('fa-pause'); 
+                                 
+        }
+    });
+
+
+});
